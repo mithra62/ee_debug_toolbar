a99f19204198b70df569f77d9d2d67d1e9c4023c
Christopher Imrie <ckimrie@gmail.com>
2012-11-11 20:20:29 +0000

    Output compatibility improvements
    Augmented the existing toolbar display output to use the native EE _display method but added some overrides that will ensure the output profiler and the template debugger are not inserted into the final output.
    
    This should allow full compatibility with complex addons that deal with caching and template output (such as Stash).

--- ext.ee_debug_toolbar.php
+++ ext.ee_debug_toolbar.php
@@ -10,7 +10,7 @@
  * @updated		1.0
  * @filesource 	./system/expressionengine/third_party/nagger/
  */
- 
+
  /**
  * EE Debug Toolbar - Extension
  *
@@ -20,20 +20,20 @@
  * @author		Eric Lamb
  * @filesource 	./system/expressionengine/third_party/ee_debug_toolbar/ext.ee_debug_toolbar.php
  */
-class Ee_debug_toolbar_ext 
+class Ee_debug_toolbar_ext
 {
 	/**
 	 * The extensions default settings
 	 * @var array
 	 */
 	public $settings = array();
-	
+
 	/**
 	 * The extension name
 	 * @var string
 	 */
 	public $name = 'EE Debug Toolbar';
-	
+
 	/**
 	 * The extension version
 	 * @var float
@@ -42,7 +42,7 @@ class Ee_debug_toolbar_ext
 	public $description	= 'Adds an unobtrusive interface for debugging output';
 	public $settings_exist	= 'n';
 	public $docs_url		= '';
-		
+
 	public function __construct($settings='')
 	{
 		$this->EE =& get_instance();
@@ -50,43 +50,43 @@ class Ee_debug_toolbar_ext
 		$this->EE->lang->loadfile('ee_debug_toolbar');
 		$this->EE->load->add_package_path(PATH_THIRD.'ee_debug_toolbar/');
 	}
-	
+
 	public function toolbar($session)
-	{	
+	{
 		$session = ($this->EE->extensions->last_call != '' ? $this->EE->extensions->last_call : $session);
-		
-		//OK, this is kind of stupid, but CI only compiles debug data if both the profiler is on and the user is Super Admin. 
+
+		//OK, this is kind of stupid, but CI only compiles debug data if both the profiler is on and the user is Super Admin.
 		if($this->EE->config->config['show_profiler'] != 'y' || $session->userdata('group_id') != '1')
 		{
 			return $session;
 		}
-		
+
 		if($this->EE->input->get("C") == "javascript"){
 			return $session;
-		}		
-		
+		}
+
 		global $EXT;
-				
+
 		//BELOW IS STOLEN FROM CHRIS IMRIE AND REQUIREJS WITH PERMISSION
 		$this->EE->load->file(PATH_THIRD . "ee_debug_toolbar/libraries/Ee_toolbar_hook.php");
-		
+
 		//We overwrite the CI_Hooks class with our own since the CI_Hooks class will always load
 		//hooks class files relative to APPPATH, when what we really need is to load RequireJS hook from the
 		//third_party folder, which we KNOW can always be found with PATH_THIRD. Hence we extend the class and
 		//simply redefine the _run_hook method to load relative to PATH_THIRD. Simples.
 		$EET_EXT = new Ee_toolbar_hook();
-		
+
 		//Capture existing hooks just in case (although this is EE - it's unlikely)
 		$EET_EXT->hooks = $EXT->hooks;
-		
+
 		//Enable CI Hooks
 		$EET_EXT->enabled = TRUE;
-		
+
 		//Create the post_controller hook array if needed
 		if(!isset($EET_EXT->hooks['post_controller'])){
 			$EET_EXT->hooks['post_controller'] = array();
 		}
-		
+
 		//Add our hook
 		$EET_EXT->hooks['display_override'][] = array(
 				'class' => __CLASS__,
@@ -95,13 +95,14 @@ class Ee_debug_toolbar_ext
 				'filepath' => "ee_debug_toolbar" ,
 				'params' => array()
 		);
-		
+
+
 		//Overwrite the global CI_Hooks instance with our modified version
 		$EXT = $EET_EXT;
-	
+
 		return $session;
 	}
-	
+
 	/**
 	 * Post EE Controller
 	 *
@@ -111,10 +112,17 @@ class Ee_debug_toolbar_ext
 	 */
 	public function modify_output()
 	{
+		//If its an AJAX request (eg: EE JS Combo loader or jQuery library load) then call it a day...
+		if(AJAX_REQUEST || (property_exists($this->EE, "TMPL") && $this->EE->TMPL->template_type == 'js')) {
+			return $this->EE->output->_display();
+		}
+
+		$this->EE->load->library('Toolbar');
+
 		//Fetch the final CP Output
 		$this->EE->benchmark->mark('ee_debug_benchmark_start');
-		$html = $this->EE->output->final_output;
-		$this->EE->load->library('Toolbar');
+
+		//Toolbar UI Vars
 		$vars = array();
 		$vars['query_count'] = $this->EE->db->query_count;
 		$vars['elapsed_time'] = $this->EE->benchmark->elapsed_time('total_execution_time_start', 'total_execution_time_end');
@@ -124,20 +132,37 @@ class Ee_debug_toolbar_ext
 		$vars['memory_usage'] = $this->EE->toolbar->filesize_format(memory_get_peak_usage());
 		$vars['template_debugging'] = (isset($this->EE->TMPL->log) ? $this->EE->TMPL->log : FALSE);
 		$vars['ext_version'] = $this->version;
-		
 		$this->EE->benchmark->mark('ee_debug_benchmark_end');
 		$vars['benchmark_data'] = $this->EE->toolbar->setup_benchmarks();
 
-		$html = str_replace('</body>', $this->EE->load->view('toolbar', $vars, TRUE).'</body>', $html);
+		$html = $this->EE->output->final_output;
 
-		//exiting like this can cause issues so it's temporary until new approach can be compiled
-		echo $this->EE->output->final_output = $html;
-		exit;
-	}	
-	
+		//Rare, but the closing body tag may not exist. So if it doesnt, append the template instead
+		//of inserting. We may be able to get away with simply always appending, but this seems cleaner
+		//even if more expensive.
+		if(strpos($html, "</body>") === FALSE) {
+			$html .= $this->EE->load->view('toolbar', $vars, TRUE);
+		} else {
+			$html = str_replace('</body>', $this->EE->load->view('toolbar', $vars, TRUE).'</body>', $html);
+		}
+
+
+		//Get CI to do its usual thing and build the final output, but we'll switch off the debugging
+		//since we have already added the debug data to the body output. Doing it this way means
+		//we should retain 100% compatibility (I'm looking at you Stash...)
+		$this->EE->output->final_output = $html;
+		$this->EE->TMPL->debugging = FALSE;
+		$this->EE->TMPL->log = FALSE;
+		$this->EE->output->enable_profiler = FALSE;
+
+		//Fist pump.
+		$this->EE->output->_display();
+
+	}
+
 	public function activate_extension()
-	{	
-		$this->settings['alert_message'] = lang('default_alert_message');			
+	{
+		$this->settings['alert_message'] = lang('default_alert_message');
 		$data = array(
 				'class'     => __CLASS__,
 				'method'    => 'toolbar',
@@ -147,29 +172,29 @@ class Ee_debug_toolbar_ext
 				'version'   => $this->version,
 				'enabled'   => 'y'
 		);
-		
-		$this->EE->db->insert('extensions', $data);		
+
+		$this->EE->db->insert('extensions', $data);
 		return TRUE;
 	}
-	
+
 	public function update_extension($current = '')
 	{
 	    if ($current == '' OR $current == $this->version)
 	    {
 	        return FALSE;
 	    }
-	
+
 	    $this->EE->db->where('class', __CLASS__);
 	    $this->EE->db->update(
 	                'extensions',
 	                array('version' => $this->version)
 	    );
 	}
-	
+
 	public function disable_extension()
 	{
 	    $this->EE->db->where('class', __CLASS__);
 	    $this->EE->db->delete('extensions');
 	}
-		
+
 }

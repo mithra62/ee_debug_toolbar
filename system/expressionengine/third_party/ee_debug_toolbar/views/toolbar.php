f071edf29018e069d4e2c5b8cd838625ff6c871f
Eric Lamb <eric@mithra62.com>
2012-12-19 13:13:11 -0700

    Moved Google Chart JS/HTML to own view script (for sanity's sake)

--- system/expressionengine/third_party/ee_debug_toolbar/views/toolbar.php
+++ system/expressionengine/third_party/ee_debug_toolbar/views/toolbar.php
@@ -126,62 +126,7 @@
 		<?php
 		if($template_debugging && is_array($template_debugging)):
 			echo "<h4>".lang('template_debugging')."</h4>"; 
-		?>
-			<div id="chart_div"></div>
-			<script type="text/javascript" src="https://www.google.com/jsapi"></script>
-			<script type="text/javascript">
-			google.load("visualization", "1", {packages:["corechart"]});
-			google.setOnLoadCallback(drawChart);
-			
-			function drawChart() {
-
-		        var data = new google.visualization.DataTable();
-		        data.addColumn('string', 'Memory');
-		        data.addColumn('number', 'Times');
-		        data.addColumn({type:'string', role:'tooltip'});
-		        data.addRows(<?php echo count($template_debugging);?>);
-		        				
-				
-				<?php
-				$parts = array(); 
-				foreach($template_debugging AS $index => $log)
-				{
-					echo "var tooltip = '".addslashes(str_replace("\n", "\\n", $log['desc']))."\\nMemory:".$log['memory']."MB\\nTime:".$log['time']."';\n";
-					echo "data.setValue($index, 0, '".$log['time']."');\n";
-					echo "data.setValue($index, 1, ".$log['memory'].");\n";
-					echo "data.setValue($index, 2, tooltip);\n";
-				}
-				echo implode(',', $parts);
-				?>
-
-				var chart_width = document.getElementById("chart_div").offsetWidth+10;
-				var area_width = chart_width+20;
-				
-				var formatter = new google.visualization.NumberFormat({suffix: 'MB'});
-		        formatter.format(data, 1);
-		        formatter.format(data, 0);	
-		        							
-				var options = {
-					title: '<?php echo lang('template_debugging'); ?>',
-					backgroundColor: 'none',
-		            width: 600, 
-		            height: 220,
-		            hAxis: {slantedText: true},
-		            legend: {position: 'none'},
-		            chartArea: {
-		            	width: 620, 
-		            	height: "160",
-		            	top: 10,
-		            	left:30
-		            } 		            
-					
-				};
-				
-				var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-				chart.draw(data, options);
-			}
-			</script>		
-		<?php 
+			$this->load->view('tmpl_graph'); 
 		else: 
 		?>
 		<h4><?php echo lang('templates_not_enabled'); ?></h4>

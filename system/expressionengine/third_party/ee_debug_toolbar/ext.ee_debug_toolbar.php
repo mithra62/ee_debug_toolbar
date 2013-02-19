d7892cc77b67ad2cbe06088b9edd8a3d7a6620bd
Eric Lamb <eric@mithra62.com>
2013-02-18 22:05:27 -0700

    Abstracted panel details into array (ready for hook manipulation Setup look for toolbar sections and details

--- system/expressionengine/third_party/ee_debug_toolbar/ext.ee_debug_toolbar.php
+++ system/expressionengine/third_party/ee_debug_toolbar/ext.ee_debug_toolbar.php
@@ -156,9 +156,6 @@ class Ee_debug_toolbar_ext
 		//starting a benchmark to make sure we're not a problem
 		$this->EE->benchmark->mark('ee_debug_benchmark_start');
 		
-		$report_data = array();
-		
-
 		//Toolbar UI Vars
 		$vars                                  = array();
 		$vars['query_count']                   = $this->EE->db->query_count;
@@ -172,23 +169,69 @@ class Ee_debug_toolbar_ext
 		$vars['template_debugging_chart_json'] = ($vars['template_debugging_enabled'] ? $this->EE->toolbar->format_tmpl_chart_json($vars['template_debugging']) : array());
 		$vars['included_file_data']            = $this->EE->toolbar->setup_files(get_included_files());
 
-		$vars['ext_version'] = $this->version;
+		$vars['ext_version']                   = $this->version;
+		$this->settings                        = $this->EE->toolbar->get_settings();
+		$vars['theme_img_url']                 = $this->EE->toolbar->create_theme_url($this->settings['theme'], 'images');
+		$vars['theme_js_url']                  = $this->EE->toolbar->create_theme_url($this->settings['theme'], 'js');
+		$vars['theme_css_url']                 = $this->EE->toolbar->create_theme_url($this->settings['theme'], 'css');
+		
+		//Setup the panel UI meta details
+		$report_info = array();
+		$report_info['master_view_script']                     = 'toolbar';
+		$report_info['panel_data']['copyright']['view_script'] = 'partials/copyright';
+		$report_info['panel_data']['copyright']['image']       = $vars['theme_img_url'].'logo.png';
+		$report_info['panel_data']['copyright']['title']       = 'v'.APP_VER.' '.phpversion();
+		$report_info['panel_data']['copyright']['data_target'] = 'EEDebug_copyright';
+		
+		$report_info['panel_data']['variables']['view_script'] = 'partials/variables';
+		$report_info['panel_data']['variables']['image']       = $vars['theme_img_url'].'variables.png';
+		$report_info['panel_data']['variables']['title']       = lang('variables');
+		$report_info['panel_data']['variables']['data_target'] = 'EEDebug_variables';	
+
+		$report_info['panel_data']['files']['view_script'] = 'partials/files';
+		$report_info['panel_data']['files']['image']       = $vars['theme_img_url'].'files.png';
+		$report_info['panel_data']['files']['title']       = count(get_included_files()).' '.lang('files');
+		$report_info['panel_data']['files']['data_target'] = 'EEDebug_file';
+		
+		$report_info['panel_data']['memory']['view_script'] = 'partials/memory';
+		$report_info['panel_data']['memory']['image']       = $vars['theme_img_url'].'memory.png';
+		$report_info['panel_data']['memory']['title']       = $vars['memory_usage'].' '.ini_get('memory_limit');
+		$report_info['panel_data']['memory']['data_target'] = ($this->EE->input->get("D", FALSE) != 'cp' ? 'EEDebug_memory' : 'EEDebug_memory_cp');		
+		
+		$report_info['panel_data']['time']['view_script'] = 'partials/time';
+		$report_info['panel_data']['time']['image']       = $vars['theme_img_url'].'time.png';
+		$report_info['panel_data']['time']['title']       = $vars['elapsed_time'].'s';
+		$report_info['panel_data']['time']['data_target'] = 'EEDebug_time';	
+
+		$report_info['panel_data']['config']['view_script'] = 'partials/config';
+		$report_info['panel_data']['config']['image']       = $vars['theme_img_url'].'config.png';
+		$report_info['panel_data']['config']['title']       = lang('config').' ('.count($vars['config_data']).')';
+		$report_info['panel_data']['config']['data_target'] = 'EEDebug_registry';	
+
+		$report_info['panel_data']['db']['view_script'] = 'partials/db';
+		$report_info['panel_data']['db']['image']       = $vars['theme_img_url'].'db.png';
+		$report_info['panel_data']['db']['title']       = $vars['query_count'].' '.lang('in').' '.$vars['query_data']['total_time'].'s';
+		$report_info['panel_data']['db']['data_target'] = 'EEDebug_database';		
+		
+		/*
+		$report_info['panel_data']['time']['view_script']      = 'partials/time';
+		$report_info['panel_data']['config']['view_script']    = 'partials/config';
+		$report_info['panel_data']['db']['view_script']        = 'partials/db';
+		*/		
+		$vars = array_merge($vars, $report_info);
+
 		$this->EE->benchmark->mark('ee_debug_benchmark_end');
 		$vars['benchmark_data'] = $this->EE->toolbar->setup_benchmarks();
-		$this->settings = $this->EE->toolbar->get_settings();
-		$vars['theme_img_url'] = $this->EE->toolbar->create_theme_url($this->settings['theme'], 'images');
-		$vars['theme_js_url'] = $this->EE->toolbar->create_theme_url($this->settings['theme'], 'js');
-		$vars['theme_css_url'] = $this->EE->toolbar->create_theme_url($this->settings['theme'], 'css');
-
+				
 		$html = $this->EE->output->final_output;
 
 		//Rare, but the closing body tag may not exist. So if it doesnt, append the template instead
 		//of inserting. We may be able to get away with simply always appending, but this seems cleaner
 		//even if more expensive.
 		if (strpos($html, "</body>") === false) {
-			$html .= $this->EE->load->view('toolbar', $vars, true);
+			$html .= $this->EE->load->view($report_info['master_view_script'], $vars, true);
 		} else {
-			$html = str_replace('</body>', $this->EE->load->view('toolbar', $vars, true) . '</body>', $html);
+			$html = str_replace('</body>', $this->EE->load->view($report_info['master_view_script'], $vars, true) . '</body>', $html);
 		}
 
 
--- system/expressionengine/third_party/ee_debug_toolbar/views/toolbar.php
+++ system/expressionengine/third_party/ee_debug_toolbar/views/toolbar.php
@@ -2,59 +2,22 @@
 <link rel="stylesheet" type="text/css" href="<?php echo $theme_css_url."ee_debug_toolbar.css" ?>">
 
 <div id="EEDebug_debug">
-	<?php $this->load->view('partials/copyright'); ?>
-	<?php $this->load->view('partials/variables'); ?>
-	<?php $this->load->view('partials/files'); ?>
-	<?php $this->load->view('partials/memory'); ?>
-	<?php $this->load->view('partials/time'); ?>
-	<?php $this->load->view('partials/config'); ?>
-	<?php $this->load->view('partials/db'); ?>
+	<?php 
+	//first let's write out our panels
+	foreach($panel_data AS $key => $value)
+	{
+		$this->load->view($value['view_script']); 
+	}
+	?>
 	
 	<div id="EEDebug_info">
-		<span class="EEDebug_span clickable" data-target="EEDebug_copyright">
-			<img src="<?php echo $theme_img_url."logo.png" ?>"
-				 style="vertical-align:middle" alt="<?php echo lang('credits'); ?>"
-				 title="<?php echo lang('credits'); ?>">  v<?=APP_VER?> <?php //echo ' - '; echo lang('build'). '&nbsp;'.APP_BUILD;?>
-			/<?php echo phpversion(); ?>
+		<?php foreach($panel_data AS $key => $value): ?>
+		<span class="EEDebug_span clickable" data-target="<?php echo $value['data_target']; ?>">
+			<img src="<?php echo $value['image']; ?>"
+				 style="vertical-align:middle" alt="<?php echo $value['title']; ?>"
+				 title="<?php echo $value['title']; ?>">  <?php echo $value['title']; ?>
 		</span>
-		<span class="EEDebug_span clickable" data-target="EEDebug_variables">
-			<img src="<?php echo $theme_img_url."variables.png" ?>"
-				 style="vertical-align:middle" alt="<?php echo lang('variables'); ?>"
-				 title="<?php echo lang('variables'); ?>">  <?php echo lang('variables'); ?>
-		</span>
-		<span class="EEDebug_span clickable" data-target="EEDebug_file">
-			<img src="<?php echo $theme_img_url."files.png" ?>"
-				 style="vertical-align:middle" alt="<?php echo lang('files'); ?>"
-				 title="<?php echo lang('files'); ?>"> <?php echo count(get_included_files()); ?> <?php echo lang('files'); ?>
-		</span>
-
-		<?php if ($this->input->get("D", FALSE) != 'cp'): ?>
-		<span class="EEDebug_span clickable" data-target="EEDebug_memory">
-		<?php else: ?>
-		<span class="EEDebug_span clickable" data-target="EEDebug_memory_cp">
-		<?php endif; ?>
-		<img src="<?php echo $theme_img_url."memory.png" ?>"
-			 style="vertical-align:middle" alt="<?php echo lang('memory'); ?>"
-			 title="<?php echo lang('memory'); ?>"> <?php echo $memory_usage; ?>
-		of <?php echo ini_get('memory_limit'); ?>
-		</span>
-		<span class="EEDebug_span clickable" data-target="EEDebug_time">
-			<img src="<?php echo $theme_img_url."time.png" ?>"
-				 style="vertical-align:middle" alt="<?php echo lang('time'); ?>"
-				 title="<?php echo lang('time'); ?>"> <?php echo $elapsed_time; ?>s
-		</span>
-		<span class="EEDebug_span clickable" data-target="EEDebug_registry">
-			<img src="<?php echo $theme_img_url."config.png" ?>"
-				 style="vertical-align:middle" alt="<?php echo lang('configuration_data'); ?>"
-				 title="<?php echo lang('configuration_data'); ?>">  <?php echo lang('config'); ?>
-			(<?php echo count($config_data); ?>)
-		</span>
-		<span class="EEDebug_span clickable" data-target="EEDebug_database">
-			<img src="<?php echo $theme_img_url."db.png" ?>"
-				 style="vertical-align:middle" alt="<?php echo lang('database'); ?>"
-				 title="<?php echo lang('database'); ?>"> <?php echo $query_count; ?> <?php echo lang('in'); ?> <?php echo $query_data['total_time']; ?>
-			s
-		</span>
+		<?php endforeach; ?>
 		<span class="EEDebug_span EEDebug_last clickable" id="EEDebug_toggler">&#171;</span>
 	</div>
 </div>

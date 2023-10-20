0fc6cf6a39c9b7f8a3d6a70bf7988998d85ef18f
Eric Lamb <eric@mithra62.com>
2023-10-19 19:05:08 -0700

    removes duplicate page render

--- system/user/addons/ee_debug_toolbar/mcp.ee_debug_toolbar.php
+++ system/user/addons/ee_debug_toolbar/mcp.ee_debug_toolbar.php
@@ -42,9 +42,8 @@ class Ee_debug_toolbar_mcp
 		
 	public function __construct()
 	{
-		$this->EE =& get_instance();
 		$path = dirname(realpath(__FILE__));
-		include $path.'/config'.EXT;
+		include $path.'/config.php';
 		$this->class = $config['class_name'];
 		$this->version = $config['version'];		
 		$this->mod_name = $config['mod_url_name'];
@@ -52,7 +51,7 @@ class Ee_debug_toolbar_mcp
 		$this->query_base = 'C=addons_modules'.AMP.'M=show_module_cp'.AMP.'module='.$this->mod_name.AMP.'method=';
 		$this->url_base = BASE.AMP.$this->query_base;		
 		
-		$this->EE->cp->set_breadcrumb(BASE.AMP.'C=addons_modules'.AMP.'M=show_module_cp'.AMP.'module='.$this->mod_name, $this->EE->lang->line('ee_debug_toolbar_module_name'));		
+		ee()->cp->set_breadcrumb(BASE.AMP.'C=addons_modules'.AMP.'M=show_module_cp'.AMP.'module='.$this->mod_name, ee()->lang->line('ee_debug_toolbar_module_name'));		
 		
 		$this->add_breadcrumb(BASE.AMP.'C=addons', lang('add_ons'));
 		$this->add_breadcrumb(BASE.AMP.'C=addons_extensions', lang('extensions'));
@@ -62,50 +61,50 @@ class Ee_debug_toolbar_mcp
 	private function add_breadcrumb($link, $title)
 	{
 		self::$_breadcrumbs[$link] = $title;
-		$this->EE->load->vars(array('cp_breadcrumbs' => self::$_breadcrumbs));
+		ee()->load->vars(array('cp_breadcrumbs' => self::$_breadcrumbs));
 	}	
 	
 	public function index()
 	{
-		$this->EE->functions->redirect($this->url_base.'settings');
+		ee()->functions->redirect($this->url_base.'settings');
 	}
 
 	public function settings()
 	{
-		$this->EE->load->library('table');
-		$this->EE->load->library('toolbar');
-		$this->settings = $this->EE->toolbar->get_settings();
+		ee()->load->library('table');
+		ee()->load->library('toolbar');
+		$this->settings = ee()->toolbar->get_settings();
 		
 		if(isset($_POST['go_settings']) && $_POST['go_settings'] == 'yes')
 		{
-			if($this->EE->debug_settings->update_settings($_POST))
+			if(ee()->debug_settings->update_settings($_POST))
 			{
-				$this->EE->logger->log_action($this->EE->lang->line('log_settings_updated'));
-				$this->EE->session->set_flashdata('message_success', $this->EE->lang->line('settings_updated'));
-				$this->EE->functions->redirect($this->url_base.'settings');
+				ee()->logger->log_ee()ion(ee()->lang->line('log_settings_updated'));
+				ee()->session->set_flashdata('message_success', ee()->lang->line('settings_updated'));
+				ee()->functions->redirect($this->url_base.'settings');
 				exit;
 			}
 			else
 			{
-				$this->EE->session->set_flashdata('message_failure', $this->EE->lang->line('settings_update_fail'));
-				$this->EE->functions->redirect($this->url_base.'settings');
+				ee()->session->set_flashdata('message_failure', ee()->lang->line('settings_update_fail'));
+				ee()->functions->redirect($this->url_base.'settings');
 				exit;
 			}
 		}
 		
-		$this->EE->view->cp_page_title = $this->EE->lang->line('settings');
+		ee()->view->cp_page_title = ee()->lang->line('settings');
 	
 		$vars = array();
 		$vars['settings'] = $this->settings;
 		$vars['query_base'] = $this->query_base;
-		$vars['available_themes'] = $this->EE->toolbar->get_themes();
-		$vars['toolbar_positions'] = $this->EE->toolbar->toolbar_positions;
+		$vars['available_themes'] = ee()->toolbar->get_themes();
+		$vars['toolbar_positions'] = ee()->toolbar->toolbar_positions;
 		$vars['settings_disable'] = FALSE;
-		if(isset($this->EE->config->config['ee_debug_toolbar']))
+		if(isset(ee()->config->config['ee_debug_toolbar']))
 		{
 			$vars['settings_disable'] = 'disabled="disabled"';
 		}
 	
-		return $this->EE->load->view('settings', $vars, TRUE);
+		return ee()->load->view('settings', $vars, TRUE);
 	}
 }

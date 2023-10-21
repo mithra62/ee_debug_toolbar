33336b2e0bbdd923fcf7c2c62ec4237f94c0ab23
Eric Lamb <eric@mithra62.com>
2023-10-21 11:33:25 -0700

    adds additional paths

--- system/user/addons/ee_debug_toolbar/views/partials/files.php
+++ system/user/addons/ee_debug_toolbar/views/partials/files.php
@@ -7,6 +7,11 @@
 <h4><?php echo lang('system_paths'); ?></h4>
 
 <span class="label"><?php echo lang('app'); ?>:</span> <code><?php echo realpath(APPPATH); ?></code><br>
+<span class="label"><?php echo lang('sysdir'); ?>:</span> <code><?php echo realpath(SYSDIR); ?></code><br>
+<span class="label"><?php echo lang('fcpath'); ?>:</span> <code><?php echo realpath(FCPATH); ?></code><br>
+<span class="label"><?php echo lang('syspath'); ?>:</span> <code><?php echo realpath(SYSPATH); ?></code><br>
+<span class="label"><?php echo lang('self'); ?>:</span> <code><?php echo realpath(SELF); ?></code><br>
+<span class="label"><?php echo lang('basepath'); ?>:</span> <code><?php echo realpath(BASEPATH); ?></code><br>
 <span class="label"><?php echo lang('themes'); ?>:</span> <code><?php echo realpath(PATH_THEMES); ?></code><br>
 <span class="label"><?php echo lang('third_party'); ?>:</span> <code><?php echo realpath(PATH_THIRD); ?></code><br>
 <span class="label"><?php echo lang('member_themes'); ?>:</span> <code><?php echo realpath(PATH_MBR_THEMES); ?></code>

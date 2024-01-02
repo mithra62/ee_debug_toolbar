0d65297415b57302290ff9567bcd4057fab5f8f4
Eric Lamb <eric@mithra62.com>
2024-01-01 18:09:54 -0700

    adds group logic to form for email ext

--- system/user/addons/eedt_email/Extensions/EeDebugToolbarSettingsForm.php
+++ system/user/addons/eedt_email/Extensions/EeDebugToolbarSettingsForm.php
@@ -15,11 +15,12 @@ class EeDebugToolbarSettingsForm extends AbstractHook
         $field_set->setDesc('eedt_email.form.desc.email_action');
         $field = $field_set->getField('email_action', 'select');
         $field->setValue($settings['email_action'])
+            ->set('group_toggle', ['log' => 'logging', 'log_send' => 'logging'])
             ->setChoices(ee('eedt_email:EmailService')->getActionOptions());
 
 
         $field_set = $field_group->getFieldSet('eedt_email.form.email_log_dir');
-        $field_set->setDesc('eedt_email.form.desc.form.email_log_dir');
+        $field_set->set('group', 'logging')->setDesc('eedt_email.form.desc.form.email_log_dir');
         $field = $field_set->getField('email_log_dir', 'text');
         $field->setValue($settings['email_log_dir']);
 

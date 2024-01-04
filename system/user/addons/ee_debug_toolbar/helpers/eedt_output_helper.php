f270920504a99e22dd500529f8a215aefe568a0b
Eric Lamb <eric@mithra62.com>
2024-01-03 19:35:21 -0700

    code formatter

--- system/user/addons/ee_debug_toolbar/helpers/eedt_output_helper.php
+++ system/user/addons/ee_debug_toolbar/helpers/eedt_output_helper.php
@@ -80,3 +80,43 @@ if (!function_exists('eedt_third_party_path')) {
         return $path;
     }
 }
+
+if (!function_exists('eedt_highlight_code')) {
+    function eedt_highlight_code($str)
+    {
+        // The highlight string function encodes and highlights
+        // brackets so we need them to start raw
+        $str = str_replace(array('&lt;', '&gt;'), array('<', '>'), $str);
+
+        // Replace any existing PHP tags to temporary markers so they don't accidentally
+        // break the string out of PHP, and thus, thwart the highlighting.
+
+        $str = str_replace(
+            array('<?', '?>', '<%', '%>', '\\', '</script>'),
+            array('phptagopen', 'phptagclose', 'asptagopen', 'asptagclose', 'backslashtmp', 'scriptclose'),
+            $str
+        );
+
+        // The highlight_string function requires that the text be surrounded
+        // by PHP tags, which we will remove later
+        $str = '<?php ' . $str . ' ?>'; // <?
+
+        // All the magic happens here, baby!
+        $str = highlight_string($str, true);
+
+        // Remove our artificially added PHP, and the syntax highlighting that came with it
+        $str = preg_replace('/<span style="color: #([A-Z0-9]+)">&lt;\?php(&nbsp;| )/i', '<span style="color: #$1">', $str);
+        $str = preg_replace('/(<span style="color: #[A-Z0-9]+">.*?)\?&gt;<\/span>\n<\/span>\n<\/code>/is', "$1</span>\n</span>\n</code>", $str);
+        $str = preg_replace('/<span style="color: #[A-Z0-9]+"\><\/span>/i', '', $str);
+
+        // Replace our markers back to PHP tags.
+        $str = str_replace(
+            array('phptagopen', 'phptagclose', 'asptagopen', 'asptagclose', 'backslashtmp', 'scriptclose'),
+            array('&lt;?', '?&gt;', '&lt;%', '%&gt;', '\\', '&lt;/script&gt;'),
+            $str
+        );
+
+        return $str;
+    }
+
+}

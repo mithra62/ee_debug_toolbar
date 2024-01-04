544bab434e9b72028cf0684dfa163582e0c24c5a
Eric Lamb <eric@mithra62.com>
2024-01-03 19:35:15 -0700

    error form heading

--- system/user/addons/eedt_errors/language/english/lang.eedt_errors.php
+++ system/user/addons/eedt_errors/language/english/lang.eedt_errors.php
@@ -10,4 +10,5 @@ $lang = [
     'eedt_errors.form.desc.hide_error_codes' => 'Which PHP Errors do you want to hide from your views? Note this only applies to Errors; uncaught Exceptions are always fatal.',
     'eedt_errors.form.error_log_path' => 'Error Log Path',
     'eedt_errors.form.desc.error_log_path' => 'The full system path to where you want to store your Error Logs. If none entered, your logs will be stored in the ExpressionEngine Cache directory',
+    'eedt_errors.form.header.settings' => 'Error Handling Settings',
 ];

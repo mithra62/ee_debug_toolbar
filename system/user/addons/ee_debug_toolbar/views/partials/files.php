b1c4374e75c685e11e3c2dc6bfbc76c07b5e0557
Eric Lamb <eric@mithra62.com>
2024-01-14 11:34:09 -0700

    adds links to templates in used template list

--- system/user/addons/ee_debug_toolbar/views/partials/files.php
+++ system/user/addons/ee_debug_toolbar/views/partials/files.php
@@ -24,7 +24,8 @@ $total_addon_files = count($included_file_data['third_party_addon']) + count($in
         echo '<h4>' . lang('eedt.templates_used') . '</h4>';
         foreach($templates_used AS $template) {
             if($template['template_name']) {
-                echo $template_groups[$template['group_id']] . '/' . $template['template_name'] . '<br>';
+                echo '<a href="' . ee()->config->item('cp_url') . '?/cp/design/template/edit/' . $template['template_id'] . '">' .
+                    $template_groups[$template['group_id']] . '/' . $template['template_name'] . '</a><br>';
             }
         }
 

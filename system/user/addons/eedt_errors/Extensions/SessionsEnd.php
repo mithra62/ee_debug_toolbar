a3cdad6b7e7728a1d7f11d8488f76c97914a4b9d
Eric Lamb <eric@mithra62.com>
2024-02-13 15:11:53 -0700

    ensures toolbar installed before error handler processing

--- system/user/addons/eedt_errors/Extensions/SessionsEnd.php
+++ system/user/addons/eedt_errors/Extensions/SessionsEnd.php
@@ -6,6 +6,11 @@ class SessionsEnd extends AbstractHook
 {
     public function process($session)
     {
+        //check for eedt installed
+        if (!ee()->db->table_exists('eedt_settings')) {
+            return $session;
+        }
+
         $settings = ee('eedt:SettingsService')->getSettings();
         if (!empty($settings['error_handler']) && $settings['error_handler'] == 'toolbar') {
             $error_handler = ee('eedt_errors:ErrorHandlerService');

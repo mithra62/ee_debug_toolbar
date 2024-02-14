06567721d0421493e29402b00dcfe18355ecb29b
Eric Lamb <eric@mithra62.com>
2024-02-13 21:48:25 -0700

    closes panel upon clear

--- themes/user/eedt_errors/js/eedt_errors.js
+++ themes/user/eedt_errors/js/eedt_errors.js
@@ -1,9 +1,7 @@
 (function () {
 
     jQuery("#Eedt_debug_eedt_errors_panel").on('click', "#EEDebug_errors_clear_errors", function () {
-        eedt.ajax('Eedt_errors', 'ClearErrorLog', {
-            'cp': eedt.config('cp') ? 'y' : 'n'
-        }).then(function(data){
+        eedt.ajax('Eedt_errors', 'ClearErrorLog', {}).then(function(data){
             jQuery("#eedt_error_content").hide();
             eedt.closePanels();
         });

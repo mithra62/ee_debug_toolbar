e8174701c473cae9bf3b632370a9f9ba2ebe7165
Eric Lamb <eric@mithra62.com>
2024-02-14 19:30:19 -0700

    lang fixes

--- system/user/addons/eedt_email/language/english/eedt_email_lang.php
+++ system/user/addons/eedt_email/language/english/eedt_email_lang.php
@@ -1,8 +1,11 @@
 <?php
 
 $lang = [
-    'eedt_email_module_name'        => 'eedt_email',
-    'eedt_email_module_description' => 'eedt_email description',
-    'eedt_email_settings'           => 'eedt_email Settings',
-    ''                            => '',
+    'eedt_email_module_name' => 'Debug Toolbar - Email',
+    'eedt_email_module_description' => 'Allows control over email during development',
+    'eedt_email.form.email_action' => 'Email Action',
+    'eedt_email.form.desc.email_action' => 'How do you want to handle emails?',
+    'eedt_email.form.email_log_dir' => 'Email Log Directory',
+    'eedt_email.form.desc.form.email_log_dir' => 'Where do you want to store emails? By default they\'ll be stored in your ExpressionEngine cache directory.',
+    'eedt_email.form.header.settings' => 'Email',
 ];
